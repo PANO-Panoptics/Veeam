8f135e87dc14b86c3d64ba01f072b44b36afe3f6
Judd Davey <daveyj@panopticsglobal.com>
2020-10-28 16:26:28 +0000

    added 2 servers are paths

--- Veeam Backup File Audit.ps1
+++ Veeam Backup File Audit.ps1
@@ -1,7 +1,5 @@
 ﻿# Veeam Audit
 
-# Servers = pano-veeam01, panglveeam03
-
 # Get Backup Files
 
 Function Get-BackupName ($FileName)
@@ -11,120 +9,148 @@ Function Get-BackupName ($FileName)
     Return $BackupName
 }
 
-$Imports = Import-Csv "C:\Users\JuddDavey\Documents\GitHub\Pano-Internal\Veeam\Veeam\ServerInfo.csv"
+$Imports = Import-Csv ".\ServerInfo.csv"
 
-foreach ($Import in $Imports[1])
+foreach ($Import in $Imports[0])
 {
     $ServerName = $Import.Name
     $Path = $Import.Path
     $FolderInclude = $Import.FolderInclude
     $FolderExclude = $Import.FolderExclude
     
-    $Files = Get-ChildItem "FileSystem::\\$ServerName\$Path\" -Recurse -Include $FolderInclude -Exclude $FolderExclude
+    $Files = Get-ChildItem "FileSystem::\\$ServerName\$Path\" -Recurse -Include "*$FolderInclude*" -Exclude "*$FolderExclude*" 
+
+    $BackupFiles = $Files | Where-Object {$_.extension -eq ".vrb" -or $_.extension -eq ".vbk" -or $_.extension -eq ".vib"}
 
 
+    $Now = Get-Date
+    $NowFormatted = get-date -Format "ddMMyyhhmmss"
+    $Retention = $Now.AddDays(-31)
+
+    $BackupObj = @()
 
 
-}
-
-
-
-
-$Files = Get-ChildItem "FileSystem::\\$Server\$DriveLetter\$Subfolder" -Recurse 
-
-$BackupFiles = $Files | Where-Object {$_.extension -eq ".vrb" -or $_.extension -eq ".vbk"}
-
-$Now = Get-Date
-$NowFormatted = get-date -Format "ddMMyyhhmmss"
-$Retention = $Now.AddDays(-31)
-
-$BackupObj = @()
-
-Foreach ($backupFile in $BackupFiles | Where-Object {$_.LastWriteTime -gt $Retention})
-{
-    $BackupFileName = $backupFile.Name
-    $NewName = Get-BackupName -FileName $BackupFileName
-
-    If ($NewName)
+    Foreach ($backupFile in $BackupFiles | Where-Object {$_.LastWriteTime -gt $Retention})
     {
-        if ($backupFile.Extension -eq ".vbk")
+        $BackupFileName = $backupFile.Name
+        $NewName = Get-BackupName -FileName $BackupFileName
+
+        If ($NewName)
         {
-            # If the backup file name had the server name in it. then
-            $Hash = @{
-                Type = "Seed"
-                Name = $BackupFile.Name
-                BackupName = $NewName
-                SizeGB = $backupFile.Length / 1024 / 1024 / 1024
+            if ($backupFile.Extension -eq ".vbk")
+            {
+                # If the backup file name had the server name in it. then
+                $Hash = @{
+                    VeeamServer = $ServerName
+                    Type = "Seed"
+                    Name = $BackupFile.Name
+                    BackupName = $NewName
+                    SizeGB = $backupFile.Length / 1024 / 1024 / 1024
         
+                }
+            }
+            elseif ($backupFile.Extension -eq ".vrb")
+            {
+                $Hash = @{
+                    VeeamServer = $ServerName
+                    Type = "Incredmental (vrb)"
+                    Name = $BackupFile.Name
+                    BackupName = $NewName
+                    SizeGB = $backupFile.Length / 1024 / 1024 / 1024
+                }
+            }
+            elseif ($backupFile.Extension -eq ".vib")
+            {
+                $Hash = @{
+                    VeeamServer = $ServerName
+                    Type = "Incredmental (vib)"
+                    Name = $BackupFile.Name
+                    BackupName = $NewName
+                    SizeGB = $backupFile.Length / 1024 / 1024 / 1024
+                }
+            }
+            else
+            {
+                Write-Error "Unknown file extension $($backupFile.Extension)"
+                $Hash = @{
+                    VeeamServer = $ServerName
+                    Type = "Unknown"
+                    BackupName = "Unknown"
+                    SizeGB = $backupFile.Length / 1024 / 1024 / 1024
+                }
             }
         }
-        elseif ($backupFile.Extension -eq ".vrb")
+        Else
         {
-            $Hash = @{
-                Type = "Incredmental"
-                Name = $BackupFile.Name
-                BackupName = $NewName
-                SizeGB = $backupFile.Length / 1024 / 1024 / 1024
-            }
-        }
-        else
-        {
-            Write-Error "Unknown file extension $($backupFile.Extension)"
-            $Hash = @{
-                Type = "Unknown"
-                BackupName = "Unknown"
-                SizeGB = $backupFile.Length / 1024 / 1024 / 1024
-            }
-        }
-    }
-    Else
-    {
-        if ($backupFile.Extension -eq ".vbk")
-        {
-            # If the backup file name does not have a server name in it
-            $Hash = @{
-                Type = "Seed"
-                Name = $BackupFile.Name
-                BackupName = ($BackupFile.Name.Split("_")[0]).trim()
-                SizeGB = $backupFile.Length / 1024 / 1024 / 1024
+            if ($backupFile.Extension -eq ".vbk")
+            {
+                # If the backup file name does not have a server name in it
+                $Hash = @{
+                    VeeamServer = $ServerName
+                    Type = "Seed"
+                    Name = $BackupFile.Name
+                    BackupName = ($BackupFile.Name.Split("_")[0]).trim()
+                    SizeGB = $backupFile.Length / 1024 / 1024 / 1024
         
+                }
+            }
+            elseif ($backupFile.Extension -eq ".vrb")
+            {
+                $Hash = @{
+                    VeeamServer = $ServerName
+                    Type = "Incredmental"
+                    BackupName = ($BackupFile.Name.Split('(\s+)-')[0]).trim() # split on <space> + - (e.g. " -") for some reason, instead of just a space character, i had to use \s+ regular expression
+                    Name = $BackupFile.Name
+                    SizeGB = $backupFile.Length / 1024 / 1024 / 1024
+                }
+            }
+            elseif ($backupFile.Extension -eq ".vib")
+            {
+                $Hash = @{
+                    VeeamServer = $ServerName
+                    Type = "Incredmental"
+                    BackupName = ($BackupFile.Name.Split('(\s+)-')[0]).trim() # split on <space> + - (e.g. " -") for some reason, instead of just a space character, i had to use \s+ regular expression
+                    Name = $BackupFile.Name
+                    SizeGB = $backupFile.Length / 1024 / 1024 / 1024
+                }
+            }
+            else
+            {
+                Write-Error "Unknown file extension $($backupFile.Extension)"
+                $Hash = @{
+                    VeeamServer = $ServerName
+                    Type = "Unknown"
+                    BackupName = "Unknown"
+                    SizeGB = $backupFile.Length / 1024 / 1024 / 1024
+                }
             }
         }
-        elseif ($backupFile.Extension -eq ".vrb")
-        {
-            $Hash = @{
-                Type = "Incredmental"
-                BackupName = ($BackupFile.Name.Split('(\s+)-')[0]).trim() # split on <space> + - (e.g. " -") for some reason, instead of just a space character, i had to use \s+ regular expression
-                Name = $BackupFile.Name
-                SizeGB = $backupFile.Length / 1024 / 1024 / 1024
-            }
-        }
-        else
-        {
-            Write-Error "Unknown file extension $($backupFile.Extension)"
-            $Hash = @{
-                Type = "Unknown"
-                BackupName = "Unknown"
-                SizeGB = $backupFile.Length / 1024 / 1024 / 1024
-            }
-        }
-    }
     
     
 
-    $BackupObj += New-Object psobject -Property $hash
+        $BackupObj += New-Object psobject -Property $hash
 
-    # Group the backups
+        # Group the backups
 
-    $VeeamReport = $BackupObj | Group-Object -Property BackupName | 
-        % {New-Object psobject -Property @{
-            BackupName = $_.Name
-            SizeGB = [math]::Round(($_.group | Measure-Object SizeGB -Sum).sum,2)
-            #ServerName = 
-        }
+        
+
     }
 
+
 }
+  # Group the backups
+$VeeamReport = $BackupObj | Group-Object -Property BackupName | 
+            % {New-Object psobject -Property @{
+                BackupName = $_.Name
+                SizeGB = [math]::Round(($_.group | Measure-Object SizeGB -Sum).sum,2)
+                #ServerName = 
+            }
+        }
+
+
+
+
+
 $VeeamReport | select BackupName, SizeGB | export-csv "Veeam Backup Size Report - $NowFormatted.csv" -NoTypeInformation
 
 Send-MailMessage -SmtpServer PANGlexch01 -To Judd@panoptics.com -From VeeamPowershell@Panoptics.com `
